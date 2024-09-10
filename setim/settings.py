2eb00c03d1321318ac0b44a2f4f30f97eddd4aaa
brjaeger <bryanmitch48@gmail.com>
2024-09-10 12:28:48 +0700

    pws

--- setim/settings.py
+++ setim/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y68t84ccqaea*%cc^^kiz3y4xvu8!1iw9-im+&&ovzcjno)q6e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bryan-mitch-setim.pbp.cs.ui.ac.id"]
 
 
 # Application definition
