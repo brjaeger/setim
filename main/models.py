ef86790aaa0b3a8a41d8be35c28762757b044b5d
brjaeger <bryanmitch48@gmail.com>
2024-09-18 11:36:58 +0700

    menyelesaikan README.md

--- main/models.py
+++ main/models.py
@@ -5,13 +5,13 @@ class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     name = models.CharField(max_length=255)
     price = models.IntegerField()
-    desctription = models.TextField()
-    
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+    description = models.TextField()
+    # mood = models.CharField(max_length=255)
+    # time = models.DateField(auto_now_add=True)
+    # feelings = models.TextField()
+    # mood_intensity = models.IntegerField()
 
-    @property
-    def is_mood_strong(self):
-        return self.name
+    # @property
+    # def is_mood_strong(self):
+    #     return self.name
+    
--- main/views.py
+++ main/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import MoodEntryForm
-from main.models import MoodEntry
-
-def create_mood_entry(request):
-    form = MoodEntryForm(request.POST or None)
-
-    if form.is_valid() and request.method == "POST":
-        form.save()
-        return redirect('main:show_main')
-
-    context = {'form': form}
-    return render(request, "create_mood_entry.html", context)
+from main.models import MoodEntry  
+from django.http import HttpResponse
+from django.core import serializers 
 
 def show_main(request):
     mood_entries = MoodEntry.objects.all()
@@ -19,7 +11,7 @@ def show_main(request):
         'npm' : '2306165742',
         'name': 'Bryan Mitch',
         'class': 'PBP A',
-        'mood_entries': mood_entries   
+        'mood_entries': mood_entries
     }
 
     products = [
@@ -40,7 +32,33 @@ def show_main(request):
         }
     ]
 
-    return render(request, "main.html", {'data' : context, 'products' : products, 'mood_entries' : mood_entries})
+    return render(request, "main.html", {'context' : context, 'products' : products, 'mood_entries' : mood_entries})
+
+def create_mood_entry(request):
+    form = MoodEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_mood_entry.html", context)
+
+def show_xml(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
 
